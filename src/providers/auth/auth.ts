87f5e83606a49299c4f30953e8acaf5e635131aa
unknown <nicehe74@naver.com>
2017-10-08 23:46:56 +0900

    dddd

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import { Injectable } from '@angular/core';
 //import { Http } from '@angular/http';
-//import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
 
 /*
   Generated class for the AuthProvider provider.
@@ -14,11 +14,21 @@ export class AuthProvider {
 
   constructor(){}
 
-  loginUser(email:string, password:string):firebase.Promise<any>{
-    return firebase.auth().signInWithEmailAndPassword(email, password);
+  loginUser(email:string, password:string):Promise<any>{
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+    .then( user => {
+      return new Promise( (resolve, reject) => {
+      if (user) {
+      user.newPropertyIamCreating = "New value I'm adding";
+      resolve(user);
+      } else {
+      reject(Error);
+      }
+      });
+      });
   }
 
-  signupUser(email:string, password:string):firebase.Promise<any>{
+  signupUser(email:string, password:string):Promise<any>{
     return firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(newUser => {
       firebase.database().ref(`/userProfile/${newUser.uid}/email`)
@@ -26,11 +36,11 @@ export class AuthProvider {
     }).catch(error => console.error(error));
   }
 
-  resetPassword(email:string):firebase.Promise<void>{
+  resetPassword(email:string):Promise<void>{
     return firebase.auth().sendPasswordResetEmail(email);
   }
 
-  logoutUser():firebase.Promise<void>{
+  logoutUser():Promise<void>{
     const userId:string = firebase.auth().currentUser.uid;
     firebase.database().ref(`/userProfile/${userId}`).off();
     return firebase.auth().signOut();
--- src/validators/email.ts
+++ src/validators/email.ts
@@ -1,6 +1,7 @@
 import { FormControl } from '@angular/forms';
 
 export class EmailValidator {
+    
     static isValid(control: FormControl){
         const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
         .test(control.value);
@@ -8,7 +9,7 @@ export class EmailValidator {
         if (re){
             return null;
         }
-        
+
         return {
             "invalidEmail": true
         };
